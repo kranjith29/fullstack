27a018912e43863cf3f9acdeb7c73554b5177713
Ranjith <sukran2930@gmail.com>
2023-04-08 00:16:35 +0530

    Removed unwanted files

---
+++ Javascript/Strings.js
@@ -0,0 +1,6 @@
+var myQuote = 'Best friends are like a mirror';
+var author = 'Joe Mockery';
+var fullQuote = myQuote + ' -- ' + author;
+var lengthOfString = fullQuote.length;
+console.log(fullQuote);
+console.log('Length of the Quote: ', lengthOfString);
